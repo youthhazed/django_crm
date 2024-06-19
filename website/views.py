bd83f60566f1c2c3bf539fe0c105896262f5c06e
youthhazed <term7ina3tor@gmail.com>
2024-06-19 11:58:14 +0300

    login logout registration done

--- website/views.py
+++ website/views.py
@@ -1,4 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You've been logged in")
+            return redirect("home")
+        else:
+            messages.warning(request, "A mistake occured, Try one more time")
+            return redirect("home")
+    else:
+        return render(request, "home.html")
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You logged out")
+    return redirect("home")
+
+
+def register_user(request):
+    form = SignUpForm()
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You've been registered in")
+            return redirect("home")
+        else:
+            messages.error(request, "An error occured during registration")
+
+    return render(request, "register.html", {'form':form})
