faea35766133e02a3786fda43c5cdc5f254d6456
youthhazed <term7ina3tor@gmail.com>
2024-06-19 13:29:47 +0300

    add, delete, update

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 
@@ -54,6 +54,44 @@ def record(request, pk):
     if request.user.is_authenticated:
         record = Record.objects.get(id=pk)
         return render(request, "record.html", {"record": record})
+    else:
+        messages.error(request, "You have to login")
+        return redirect("home")
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        del_record = Record.objects.get(id=pk)
+        del_record.delete()
+        messages.success(request, "You deleted the record")
+        return redirect("home")
+    else:
+        messages.error(request, "You have to login")
+        return redirect("home")
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if form.is_valid():
+            add_record = form.save()
+            messages.success(request, f"Record {add_record.first_name} was added")
+            return redirect("home")
+        return render(request, "add_record.html", {"form": form})
+    else:
+        messages.error(request, "You have to login")
+        return redirect("home")
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=record)
+        if form.is_valid():
+            updated_record = form.save()
+            messages.success(request, f"Record '{updated_record.first_name}' was added")
+            return redirect("home")
+        return render(request, "update_record.html", {"form": form})
     else:
         messages.error(request, "You have to login")
         return redirect("home")
